737ec0a9ad7bf2e3edbbd68719923c9a6ca36ff0
Esther-qu33n <queenkinyanjui107@gmail.com>
2024-10-16 23:19:15 +0300

    Add files via upload

---
+++ 2Darray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int i,j;
+  int marks[2][3]={{10,5,1},{1,5,10}};
+  for(i=0;i<2;i++){
+    for (j=0;j<3;j++){
+  printf("marks[%d][%d]=%d\n",i,j,marks[i][j]);
+}
+}
+return 0;
+}
+
+
+// Language:C 
+// ProjectType:0 
+// Copy the full code and open the CCoder APP to run it. 
+// CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
