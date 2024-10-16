9d378e23fb470b3f12f41041f2e1f56c6f6dbe4f
Esther-qu33n <queenkinyanjui107@gmail.com>
2024-10-16 23:09:05 +0300

    Add files via upload

---
+++ 1Darray.c
@@ -0,0 +1,20 @@
+//c arrays
+/*Name:Queen Esther Kinyanjui
+Ref.no:CT101/G/22453/24*/
+
+#include<stdio.h>
+
+int main(){
+  int i;
+  int marks[5]={5,10,15,20,25};
+  for(i=0;i<5;i++)
+  {printf("marks [%d]= %d\n",i,marks [i]);
+  }
+  return 0;
+  }
+
+
+// Language:C 
+// ProjectType:0 
+// Copy the full code and open the CCoder APP to run it. 
+// CCoder APP download link：https://play.google.com/store/apps/details?id=com.ikou.ccoding 
